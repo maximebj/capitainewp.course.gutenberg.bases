362a70a021afcf5a67c419edf7ddeefb317d1d7c
Maxime BERNARD-JACQUET <maxime@dysign.fr>
2020-12-31 10:15:00 +0100

    Remove build file from versioning

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '7d71b3b7b47cdaf63ef67cb55b5ca555');
