47a7f3153d2a6b4c078904465e253e58a0c7eeba
Maxime BERNARD-JACQUET <maxime@dysign.fr>
2020-12-31 10:57:10 +0100

    Styles fix in example 12

--- src/12-inspector/index.js
+++ src/12-inspector/index.js
@@ -1,7 +1,7 @@
 import { registerBlockType } from '@wordpress/blocks'
 import { __ } from '@wordpress/i18n'
 
-import './style.scss' // On importe la feuille de style du bloc
+import './style.scss'
 
 import Edit from './edit'
 import save from './save'
