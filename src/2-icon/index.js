d6334548414b6775359d25fe23bdbff5e6665eeb
Maxime BERNARD-JACQUET <maxime@dysign.fr>
2022-05-16 11:03:22 +0200

    Removed useless .js in import

--- src/2-icon/index.js
+++ src/2-icon/index.js
@@ -4,7 +4,7 @@ import './style.scss'
 
 import Edit from './edit'
 import save from './save'
-import myIcon from './custom-icon.js' // On stocke l'icône dans un fichier à part
+import myIcon from './custom-icon' // On stocke l'icône dans un fichier à part
 
 registerBlockType( 'capitainewp/icon', {
 	icon: myIcon,
