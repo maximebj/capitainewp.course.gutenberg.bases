27afef40341f913c4b524da99b45108d543858f7
François de Cambourg <francois@finepress.net>
2024-01-08 11:43:32 +0100

    updatev3 - change api v on media block + add non tested inspector block

---
+++ src/12-inspector/edit.js
@@ -0,0 +1,118 @@
+import { __ } from '@wordpress/i18n'
+import {
+	useBlockProps,
+	RichText,
+	BlockControls,
+	AlignmentToolbar,
+	InspectorControls,
+	PanelColorSettings,
+} from '@wordpress/block-editor'
+import {
+	PanelBody,
+	ButtonGroup,
+	Button,
+	ToggleControl,
+	RangeControl,
+} from '@wordpress/components'
+import { Fragment } from '@wordpress/element'
+
+import './editor.scss'
+
+export default function Edit( props ) {
+	const blockProps = useBlockProps()
+
+	return (
+		<Fragment>
+
+			<BlockControls>
+				<AlignmentToolbar
+					value={ props.attributes.alignment }
+					onChange={ alignment => props.setAttributes( { alignment: alignment } ) }
+				/>
+			</BlockControls>
+
+			<InspectorControls>
+				<PanelBody title={ __( 'Chapter sign', 'capitainewp-gut-bases' ) }>
+					<ButtonGroup>
+						{ ['#', 'n°', '§'].map( (sign) => (
+							<Button
+								isLarge
+								isPrimary={ props.attributes.chapterSign == sign }
+								onClick={ () => props.setAttributes( { chapterSign: sign } ) }
+							>
+								{ sign }
+							</Button>
+						) ) }
+					</ButtonGroup>
+				</PanelBody>
+
+				<PanelColorSettings
+					title={ __( 'Colors', 'capitainewp-gut-bases' ) }
+					colorSettings={ [
+						{
+							value: props.attributes.textColor,
+							onChange: textColor => props.setAttributes( { textColor } ),
+							label: __( 'Title color', 'capitainewp-gut-bases' ),
+						},
+						{
+							value: props.attributes.backgroundColor,
+							onChange: backgroundColor => props.setAttributes( { backgroundColor } ),
+							label: __( 'Background color', 'capitainewp-gut-bases' ),
+						},
+					] }
+				/>
+
+				<PanelBody title={ __( 'Border', 'capitainewp-gut-bases' ) }>
+					<ToggleControl
+						label={ __( 'Radius', 'capitainewp-gut-bases' ) }
+						checked={ props.attributes.withRadius }
+						onChange={ () => props.setAttributes( { withRadius: ! props.attributes.withRadius } ) }
+					/>
+
+					{ props.attributes.withRadius && (
+						<RangeControl
+							value={ props.attributes.radius }
+							onChange={ radius  => props.setAttributes( { radius } ) }
+							min={ 0 }
+							max={ 30 }
+							beforeIcon="arrow-down"
+							afterIcon="arrow-up"
+						/>
+					)}
+				</PanelBody>
+			</InspectorControls>
+
+			<div { ...blockProps }
+				style={ {
+					borderRadius: props.attributes.withRadius ? props.attributes.radius : null,
+					backgroundColor: props.attributes.backgroundColor,
+					textAlign: props.attributes.alignment,
+				} }
+			>
+				<p
+					className="first-line"
+					style={ {
+						color: props.attributes.textColor
+					} }
+				>
+					<span>{props.attributes.chapterSign}</span>
+					<RichText
+						tagName="span"
+						placeholder="1"
+						value={ props.attributes.number }
+						className="number"
+						onChange={ number => props.setAttributes( { number } ) }
+					/>
+				</p>
+				<RichText
+					tagName="h2"
+					placeholder={ __( 'Your title here', 'capitainewp-gut-bases' ) }
+					value={ props.attributes.title }
+					className="title"
+					onChange={ title => props.setAttributes( { title } ) }
+				/>
+			</div>
+
+		</Fragment>
+	)
+}
---
+++ src/12-inspector/save.js
@@ -0,0 +1,37 @@
+import { __ } from '@wordpress/i18n'
+import { useBlockProps, RichText } from '@wordpress/block-editor'
+
+export default function save( props ) {
+	const blockProps = useBlockProps.save()
+
+	return (
+		<div { ...blockProps }
+			style={ {
+				borderRadius: props.attributes.withRadius ? props.attributes.radius : null,
+				backgroundColor: props.attributes.backgroundColor,
+				textAlign: props.attributes.alignment,
+			} }
+		>
+			<p
+				className="first-line"
+				style={ { color: props.attributes.textColor } }
+			>
+				<RichText.Content
+					tagName="span"
+					className="sign"
+					value={ props.attributes.chapterSign }
+				/>
+				<RichText.Content
+					tagName="span"
+					className="number"
+					value={ props.attributes.number }
+				/>
+			</p>
+			<RichText.Content
+				tagName="h2"
+				className="title"
+				value={ props.attributes.title }
+			/>
+		</div>
+	)
+}
